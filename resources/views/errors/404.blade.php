9db8810db02920dc9540f6c4225c116128dbb3ee
mshariqq <shariqq.com@gmail.com>
2022-05-28 09:30:47 +0530

    404 not found page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,37 +4,20 @@
 @section('code', '404')--}}
 
 @section('message')
-    <style>
-        .for-margin {
-            margin: auto;
-
-            margin-bottom: 10%;
-        }
-
-        .for-margin {
-
-        }
-
-        .page-not-found {
-            margin-top: 30px;
-            font-weight: 600;
-            text-align: center;
-        }
-    </style>
-    <div class="container ">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 for-margin">
-            <div class="for-image">
-                <img style="" src="{{asset("storage/app/public/png/404.png")}}" alt="">
+    <div class="container" style="width: 100%;">
+        <div class="row">
+            <div class="col-md-12 col-12 text-left">
+                <div class="card col shadow">
+                    <div class="card-header">
+                        Not Found
+                    </div>
+                    <div class="card-body">
+                        <h2>404</h2>
+                        <p>The page your are looking doesn't exist</p>
+                        <a href="{{url('/')}}" class="btn btn-primary btn-round"> <i class="fa fa-home"></i> Go to Home</a>
+                    </div>
+                </div>
             </div>
-            <h2 class="page-not-found">{{\App\CPU\translate('Page Not found')}}</h2>
-
-            <p style="text-align: center;">{{\App\CPU\translate('We are sorry, the page you requested could not be found')}} <br> {{\App\CPU\translate('Please go back to the homepage')}}</p>
-            <div style="text-align: center;">
-                <a class="btn btn-primary" href="{{ route('home') }}"> {{\App\CPU\translate('HOME')}}</a>
-            </div>
-
         </div>
-        <div class="col-md-3"></div>
     </div>
 @endsection
